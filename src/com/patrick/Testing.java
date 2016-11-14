7dfd454dd56f79cc2ab7ebe7403fc1014245474d
Patrick <ox6185ys@go.minneapolis.edu>
2016-11-14 13:35:56 -0600

    Monday Finalize

--- src/com/patrick/Testing.java
+++ src/com/patrick/Testing.java
@@ -1,6 +1,11 @@
 package com.patrick;
-
+/*Credits for getting this Agram program working.
+        Clara's github
+        Malcolm Leehan
+        Mike Dodge
+        https://github.com/minneapolis-edu/Java2545examples/tree/master/src
+        https://docs.oracle.com/javase/7/docs/api/java/util/ArrayList.html*/
 
 public class Testing {
-
+    //This was used to run different ideas before ruining the existing program.
 }
