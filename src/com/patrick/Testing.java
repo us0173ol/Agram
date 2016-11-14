704d6b0a7d590aeb5a75c280ca4f43c162e78240
Patrick <ox6185ys@go.minneapolis.edu>
2016-11-14 13:56:14 -0600

    Monday Finalize

--- src/com/patrick/Testing.java
+++ src/com/patrick/Testing.java
@@ -1,11 +1,13 @@
 package com.patrick;
+
 /*Credits for getting this Agram program working:
         Clara James' Blackjack program is the base.
         Malcolm Leehan.
         Mike Dodge.
         Mason Elmore.
         https://github.com/minneapolis-edu/Java2545examples/tree/master/src
-        https://docs.oracle.com/javase/7/docs/api/java/util/ArrayList.html*/
+        https://docs.oracle.com/javase/7/docs/api/java/util/ArrayList.html
+        https://www.tutorialspoint.com/java/java_collections.htm*/
 
 public class Testing {
     //This class was used to run different ideas before ruining the existing program.
